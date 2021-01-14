6ff727b0405948d7114b1fffd52a97915fed1243
joaodgv <joaodiogo.veloso@gmail.com>
2021-01-14 13:23:18 +0000

    creating a simple console test program

---
+++ main/go.mod
@@ -0,0 +1,5 @@
+module main
+
+go 1.15
+
+require github.com/joaodgv/mtglib v0.1.1
--- mtglib.go
+++ mtglib.go
@@ -8,40 +8,4 @@ type Card struct {
 	COST int
 }
 
-type Database struct {
-	CARDS []Card{}
-}
-
 // FUNCTIONS
-// function to open the database
-//if there is none should create new files
-func OpenDatabase() (Database, bool) {
-
-
-	// Need to return a pointer(&)
-}
-
-// function to close the database
-func (d *Database) CloseDatabase() bool {
-
-}
-
-// function to add a new card instance
-func (d *Database) AddCard() bool {
-
-}
-
-// function to remove a card instance
-func (d *Database) RemoveCard() bool {
-
-}
-
-// function that returns a card instance
-func (d *Database) getCard() Card bool {
-
-}
-
-// function to change the count of a card
-func (d *Database) ChangeCount(name string, number int) {
-
-}
